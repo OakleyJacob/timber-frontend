865bf45eda6468f4afa1a2e8b9df5756ecbc0ba4
Chris-Frey <chris.frey@live.com>
2023-06-27 16:36:57 -0700

    removes commented out code from header.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,49 +9,24 @@ const Footer = () => {
   return (
     <>
     <Navbar
-    className="my-2"
-    color="secondary"
-    dark
-  >
-    {/* <NavbarBrand href="/">
-      <img
-        alt="logo"
-        src="/logo-white.svg"
-        style={{
-          height: 40,
-          width: 40
-        }}
-      />
-    </NavbarBrand>
-  </Navbar>
-  <Navbar
-    className="my-2"
-    color="secondary"
-    dark
-  >
-    <NavbarBrand href="/">
-      Reactstrap
-    </NavbarBrand>
-  </Navbar>
-  <Navbar
-    className="my-2"
-    color="dark"
-    dark
-  > */}
-    <NavbarBrand href="/">
-      <img
-        alt="logo"
-        src={logo}
-        style={{
-          height: 60,
-          width: 60,
-          mixBlendMode: 'multiply',
-          filter: 'brightness(125%)',
-        }}
-      />
-      JC Mingle
-    </NavbarBrand>
-  </Navbar>
+      className="my-2"
+      color="secondary"
+      dark
+      >
+      <NavbarBrand href="/">
+        <img
+          alt="logo"
+          src={logo}
+          style={{
+            height: 60,
+            width: 60,
+            mixBlendMode: 'multiply',
+            filter: 'brightness(125%)',
+          }}
+        />
+        JC Mingle
+      </NavbarBrand>
+    </Navbar>
   </>
   )
 }
